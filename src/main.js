656cf5074bb053835a9d76701b6371e64c52cdb0
Muhammed Rashid M P <alrashid4star@gmail.com>
2022-09-01 17:07:42 +0530

    file format

--- src/main.js
+++ src/main.js
@@ -1,39 +1,36 @@
-  var firstInput = document.getElementById('firstNumber');
-  var secondInput = document.getElementById('secondNumber');
-  var result = document.getElementById('result');
+var firstInput = document.getElementById('firstNumber');
+var secondInput = document.getElementById('secondNumber');
+var result = document.getElementById('result');
 
-
-
-
-function add(){
+function add() {
   // alert('Addition clicked')
   var firstNumber = parseInt(firstInput.value);
   var secondNumber = parseInt(secondInput.value);
   result.value = firstNumber + secondNumber;
 }
 
-function sub(){
+function sub() {
   // alert('Substraction clicked')
   var firstNumber = parseInt(firstInput.value);
   var secondNumber = parseInt(secondInput.value);
- result.value = firstNumber - secondNumber;
+  result.value = firstNumber - secondNumber;
 }
 
-function multiply(){
+function multiply() {
   // alert('Multiplication clicked')
   var firstNumber = parseInt(firstInput.value);
   var secondNumber = parseInt(secondInput.value);
   result.value = firstNumber * secondNumber;
 }
 
-function divide(){
+function divide() {
   // alert('Division clicked')
   var firstNumber = parseInt(firstInput.value);
   var secondNumber = parseInt(secondInput.value);
   result.value = firstNumber / secondNumber;
 }
 
-function reset(){
+function reset() {
   // alert('Clear clicked')
   firstInput.value = 0;
   secondInput.value = 0;
